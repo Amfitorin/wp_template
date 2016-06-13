ca25f8bbb0e56e4f791c933e2b7b6481e49b08ad
Amfitorin <Amfitorin@yandex.ru>
2016-06-14 00:18:50 +0300

    update

--- scripts/common.js
+++ scripts/common.js
@@ -1,19 +1,36 @@
 $(document).ready(function() {
 
-	$('#job-carousel').jcarousel({
-		animation:'slow',
-		visible: 9,
-		scroll: 1,
-		wrap: 'circular'
-	});
+    var visible = 9;
+    var count = $('#job-carousel ul li').length;
+    if (visible > count) {
+        var arrayse = $('#job-carousel ul li').toArray();
+        i = 0;
+        while (count <= visible) {
+            if (i === arrayse.length)
+                i = 0;
+            var item = $('#job-carousel ul li:eq(' + i + ')').clone();
+            $('#job-carousel ul').append(item);
+            i++;
+            count++;
+        }
+    }
 
-	$(' .sliderjob .btn-prev').click(function() {
-		$('#job-carousel').jcarousel('scroll', '-=1');
-	});
+    $('#job-carousel').jcarousel({
+        animation: 'slow',
+        visible: 9,
+        scroll: 1,
+        wrap: 'circular'
+    });
 
-	$('.sliderjob .btn-next').click(function() {
-		$('#job-carousel').jcarousel('scroll', '+=1');
-	});
+    $(' .sliderjob .btn-prev').click(function() {
+        $('#job-carousel ul').animate({
+            left: '+=' + width
+        }, 200);
+    });
 
-	
+    $('.sliderjob .btn-next').click(function() {
+        $('#job-carousel').jcarousel('scroll', '+=1');
+    });
+
+
 });
